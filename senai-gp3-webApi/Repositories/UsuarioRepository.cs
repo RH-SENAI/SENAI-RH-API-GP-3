17eef2c6d59be7696534536587c5c7b659b9ae8a
Leal-Matheus <theusleal04@hotmail.com>
2022-05-20 13:37:33 -0300

    fix: Arruma ranking

--- senai-gp3-webApi/Repositories/UsuarioRepository.cs
+++ senai-gp3-webApi/Repositories/UsuarioRepository.cs
@@ -395,32 +395,7 @@ namespace senai_gp3_webApi.Repositories
 
         }
 
-        public List<Usuario> Ranking()
-        {
-            return ctx.Usuarios
-                
-                //IdTipoUsuario seja igual ao de funcionario
-                .Where(u => u.IdTipoUsuario == 1)
-                .OrderByDescending(u => u.Trofeus)
 
-                //Seleciona os dados que serao enviados na resposta
-                .Select(u => new Usuario()
-                {
-                    IdUsuario = u.IdUsuario,
-                    Nome = u.Nome,
-                    Email = u.Email,
-                    DataNascimento = u.DataNascimento,
-                    SaldoMoeda = u.SaldoMoeda,
-                    Trofeus = u.Trofeus,
-                    IdUnidadeSenai = u.IdUnidadeSenai,
-                    IdUnidadeSenaiNavigation = new Unidadesenai()
-                    {
-                        NomeUnidadeSenai = u.IdUnidadeSenaiNavigation.NomeUnidadeSenai,
-                        TelefoneUnidadeSenai = u.IdUnidadeSenaiNavigation.TelefoneUnidadeSenai,
-                        EmailUnidadeSenai = u.IdUnidadeSenaiNavigation.EmailUnidadeSenai
-                    }
-                }).ToList();
-        }
 
         /// <summary>
         /// Valida senha 
@@ -453,13 +428,15 @@ namespace senai_gp3_webApi.Repositories
             return ctx.Usuarios
                 //IdTipoUsuario seja igual ao de funcionario
                 .Where(u => u.IdTipoUsuario == 3)
-                .OrderByDescending(u => u.MediaAvaliacao)
+                .OrderByDescending(u => u.MedSatisfacaoGeral)
                 //Seleciona os dados que serao enviados na resposta
                 .Select(u => new Usuario()
                 {
                     IdUsuario = u.IdUsuario,
                     MediaAvaliacao = u.MediaAvaliacao,
                     Nome = u.Nome,
+                    CaminhoFotoPerfil = u.CaminhoFotoPerfil,
+                    MedSatisfacaoGeral = u.MedSatisfacaoGeral,
                     Email = u.Email,
                     DataNascimento = u.DataNascimento,
                     SaldoMoeda = u.SaldoMoeda,
